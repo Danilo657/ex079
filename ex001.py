288d8dbdaa816537dbc42030e2e21721d8216684
Danilo dantas <danilodantas1560@gmail.com>
2021-08-09 00:31:46 -0300

    quinto commit

---
+++ ex001.py
@@ -0,0 +1,7 @@
+nome = str(input('diga seu nome:')).strip().upper()
+if nome == 'juan':
+    print('Eai macaco')
+elif nome == 'toddy':
+    print('seu nome es lindo')
+else:
+    print(f'seu nome é comum,{nome}')
---
+++ ex079.py
@@ -0,0 +1,13 @@
+numeros = list()
+resp = ''
+while True:
+    valores = int(input('diga um valor: '))
+    if valores not in numeros:
+        numeros.append(valores)
+        print('valor inserido  com sucessos')
+    else:
+        print('valor duplicado !!!')
+    resp = str(input('Quer continuar ? Sim ou Não ?')).strip().upper()[0]
+    if resp == 'N10':
+        break
+print(f'Os valores ditos foram {sorted(numeros)}')
