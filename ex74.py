6c239e0f6aca1be791c6732c9b713e178f08bbc7
Danilo dantas <danilodantas1560@gmail.com>
2021-08-11 15:53:05 -0300

    sorteando 5 numeros, e dizendo qual foi o maior valor e o menor

---
+++ ex74.py
@@ -0,0 +1,5 @@
+from random import randint
+num = (randint(2,9),randint(2,9),randint(2,9),randint(2,9),randint(2,9))
+print(f' os numeros sorteados foram {num}')
+print(f' o maior numero foi {max(num)}')
+print(f' o menor numero foi {min(num)}')
